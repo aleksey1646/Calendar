82c0027e7c13b218413108c497aeb6fc845ed03d
Katushka Mazalova <brunetka__93@mail.ru>
2015-03-21 17:34:05 +0300

    design the places page and edit search button

--- emptyProject/PlaceNoteViewController.m
+++ emptyProject/PlaceNoteViewController.m
@@ -45,14 +45,14 @@
 
 
 - (void) changeZoomWith:(int)zoomLevel {
-  
+    
     if (circleView) {
         float radius = circleView.layer.cornerRadius;
         
         
-      //  AppDelegate* app=[[UIApplication sharedApplication] delegate];
+        //  AppDelegate* app=[[UIApplication sharedApplication] delegate];
         //CLLocation * last_loc= [[app locationProvider]lastCoordinates];
-
+        
         
         /*
          150 DPI = 150 PPI = 150 пикселей на 1 дюйм = 150 пикселей на 2,54 см = 150 пикселей на 0,0254 м
@@ -69,7 +69,7 @@
          +0,00007791411 * +72223,822090 = +5,62725481894
          
          
-        326 - айфон
+         326 - айфон
          401 - если айфон 6+
          132 - айпад
          264 ppi retina айпад (mini retina 326 ppi)
@@ -85,23 +85,30 @@
         float metresScale = 564.24861 * pow(2, 21 - zoomLevel);
         
         float OnePixelDistanceInMeters =  (inchMetres)/ppi*metresScale;
-      
+        
         int result = lroundf((float)radius/100*75*OnePixelDistanceInMeters);
         radiusCircle = result;
         
-      int resultMOrKm = result>=1000?lroundf((float)result/1000):result;
-        NSMutableString *strFormat = [NSMutableString stringWithFormat:@"In radius: %d ",resultMOrKm];
+        int resultMOrKm = result>=1000?lroundf((float)result/1000):result;
+        
+        
+        
+        NSMutableString *strFormat = [NSMutableString stringWithString:[GLang getString:@"Places.search"]];
+        [strFormat appendString:[NSString stringWithFormat:@"%d ",resultMOrKm]];
+        
+        //[NSMutableString stringWithFormat:@"In radius: %d ",resultMOrKm];
+        
         if (result>=1000) {
-            [strFormat appendString:@"km"];
+            [strFormat appendString:@"km."];
         } else {
-            [strFormat appendString:@"m"];
+            [strFormat appendString:@"m."];
         }
         
         
-        
+        [circleRadiusView setTextAlignment:NSTextAlignmentCenter];
         [circleRadiusView setText:strFormat];
         NSLog(@"circleRadiusView %@",[circleRadiusView text]);
-       
+        
         
         
     }
@@ -110,15 +117,15 @@
 
 - (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
     NSLog(@"didRotateFromInterfaceOrientation");
-     [self addCircleView];
-    
+    [self addCircleView];
+    [self.mapWebView bringSubviewToFront:clearDarkView];
     
     if (UIDeviceOrientationIsLandscape([UIDevice currentDevice].orientation)) {
-        [circleRadiusView setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:12]];
+        [circleRadiusView setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:9]];
     }
     
-
-     [self changeZoomWith:zoom];
+    
+    [self changeZoomWith:zoom];
     [self clearMarkers];
     
     if (![self.searchBar.text isEqualToString:@""]) {
@@ -135,11 +142,11 @@
         }
     }
     
-        
     
     
     
-
+    
+    
 }
 
 - (void) addCircleView {
@@ -151,7 +158,13 @@
     if (circleRadiusView) {
         [circleRadiusView removeFromSuperview];
     }
-   
+    if (circleView.subviews) {
+        for (int i = 0; i<circleView.subviews.count; i++) {
+            UIView *view = [circleView.subviews objectAtIndex:i];
+            [view removeFromSuperview];
+        }
+    }
+    
     float size = self.mapWebView.frame.size.width > self.mapWebView.frame.size.height? self.mapWebView.frame.size.height: self.mapWebView.frame.size.width;
     
     
@@ -159,7 +172,25 @@
     
     circleView.layer.cornerRadius = circleView.frame.size.width/2;
     
-    circleView.backgroundColor = [UIColor colorWithRed:0 green:0.5 blue:1 alpha:0.2];
+    circleView.backgroundColor = [UIColor colorWithRed:0 green:0.5 blue:1 alpha:0.13];
+    
+    
+    circleView.layer.borderWidth = 0.7f;
+    circleView.layer.borderColor = [UIColor colorWithRed:0 green:0.5 blue:1 alpha:0.3].CGColor;
+    circleView.clipsToBounds = YES;
+    //circleView.layer.contentsCenter.origin.x
+    CGRect rect = CGRectMake(self.mapWebView.bounds.size.width/2-circleView.layer.cornerRadius/16, self.mapWebView.bounds.size.height/2+circleView.layer.cornerRadius/16, circleView.layer.cornerRadius/8, circleView.layer.cornerRadius/8);
+    
+    // CGRect rect = CGRectMake(self.mapWebView.frame.size.width/2-circleView.layer.cornerRadius/8, self.mapWebView.frame.size.height/2-circleView.layer.cornerRadius/8, circleView.layer.cornerRadius/8, circleView.layer.cornerRadius/8);
+    
+    
+    UIView *smalCircle = [[UIView alloc]initWithFrame:rect];
+    [smalCircle setCenter:CGPointMake(circleView.bounds.origin.x+circleView.layer.cornerRadius, circleView.bounds.origin.y+circleView.layer.cornerRadius)];
+    
+    // smalCircle.center = circleView.center;
+    
+    smalCircle.layer.cornerRadius = smalCircle.frame.size.width/2;
+    smalCircle.backgroundColor = [UIColor colorWithRed:0 green:0.5 blue:1 alpha:0.5];
     
     
     circleRadiusView = [[UILabel alloc]initWithFrame:CGRectZero];
@@ -168,7 +199,12 @@
     [circleRadiusView setFrame:CGRectMake(circleView.frame.origin.x+circleView.frame.size.width/4, circleView.frame.origin.y-sizeLabel, circleView.frame.size.width/2, self.mapWebView.frame.size.width > self.mapWebView.frame.size.height? sizeLabel: sizeLabel-sizeLabel/3)];
     circleRadiusView.backgroundColor = [UIColor whiteColor];
     circleRadiusView.textColor = [UIColor grayColor];
-    [circleRadiusView setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:17]];
+    [circleRadiusView setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:13]];
+    circleRadiusView.layer.cornerRadius = 7;
+    circleRadiusView.layer.borderWidth = 0.7f;
+    circleRadiusView.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    circleRadiusView.clipsToBounds = YES;
+    
     
     
     
@@ -179,6 +215,8 @@
     
     [self.mapWebView addSubview:circleView];
     circleView.userInteractionEnabled=NO;
+    // smalCircle.center = circleView.center;
+    [circleView addSubview:smalCircle];
     
     
     if (clearDarkView) {
@@ -212,10 +250,11 @@
     
     [clearDarkView setHidden:YES];
     [self.mapWebView addSubview:clearDarkView];
-
     
     
-
+    
+    
+    
 }
 
 - (void) setTitleInfoWith:(NSString *)click_id {
@@ -232,29 +271,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-      [_searchBar setPlaceholder:[GLang getString:@"Notes.search.placeholder"]];
+    [_searchBar setPlaceholder:[GLang getString:@"Notes.search.placeholder"]];
     
     // Do any additional setup after loading the view, typically from a nib.
     
     self.mapWebView.userInteractionEnabled = NO;
     self.mapWebView.scalesPageToFit = YES;
     [self.mapWebView setDelegate:self];
-
+    
     self.mapWebView.scrollView.delegate = self;
     
     
     
-   
+    
     
     [self.useSwitch setOn:NO];
     
-   
+    
     if((self.note.category)&&(![self.useSwitch isOn])) {
         
         [self setCommandAndParamsWithName:self.searchBar.text andCategory:self.note.category];
         
     }
-  
+    
 }
 
 - (void)panGestureChanged:(UIPanGestureRecognizer *)panGesture
@@ -270,11 +309,11 @@
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
     
-   // internal://grope.io/bounds_changed/ZOOM/LONG/LAT
+    // internal://grope.io/bounds_changed/ZOOM/LONG/LAT
     
     AppDelegate* app=[[UIApplication sharedApplication] delegate];
     CLLocation * last_loc= [[app locationProvider]lastCoordinates];
-   
+    
     
     if([[[request URL]scheme] isEqualToString:@"internal"]){
         NSString* path=[[request URL]path];
@@ -293,12 +332,12 @@
                 
             }
         }
-       
+        
         else if([path isEqualToString:@"/create_map_compelete"]){
-           
+            
             [self insertMarkerToMap:last_loc.coordinate text:@"Your here" click_id:@"coords#iam" title:@"You here"];
         }else if([path isEqualToString:@"/fully_loaded"]){
-          
+            
             
             [self changeZoomWith:mapRadius];
             
@@ -337,7 +376,7 @@
                     
                     [self setCommandAndParamsWithName:_searchBar.text andCategory:@""];
                 }
-            
+                
             }
             else if ([path  hasPrefix:@"/click_to_marker"]) {
                 
@@ -351,10 +390,10 @@
                 NSString *cl_id = [arraySeparate objectAtIndex:2];
                 
                 [self setTitleInfoWith:cl_id];
-               
+                
                 
             }
-           // [[[UIAlertView alloc]initWithTitle:@"Alert" message:[[request URL]path] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil]show];
+            // [[[UIAlertView alloc]initWithTitle:@"Alert" message:[[request URL]path] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil]show];
         }
         
         return NO;
@@ -366,7 +405,7 @@
         return false;
     }
     return true;
-
+    
     
 }
 
@@ -386,12 +425,12 @@
     // finished loading, hide the activity indicator in the status bar
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
     [self.mapWebView setUserInteractionEnabled: YES];
-   
+    
     
 }
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
     
-     NSLog(@"didFailLoadWithError %@",error);
+    NSLog(@"didFailLoadWithError %@",error);
     
     
 }
@@ -410,7 +449,7 @@
     NSLog(@"koncheniy swith is hnached %i", sender.isOn);
     if (!sender.isOn) {
         [clearDarkView setHidden:YES];
-       // [self.mapWebView bringSubviewToFront:clearDarkView];
+        // [self.mapWebView bringSubviewToFront:clearDarkView];
         
     } else {
         [clearDarkView setHidden:NO];
@@ -429,22 +468,22 @@
     if ([responce isKindOfClass:[NSDictionary class]]) {
         return;
     }
-  
+    
     NSArray *arrayResp= responce;
     
-  
-        for (int i = 0; i<[arrayResp count]; i++) {
-            
-            NSDictionary *dictResponce = [arrayResp objectAtIndex:i];
-            NSDictionary * loc_dict=[[dictResponce objectForKey:@"geometry"] objectForKey:@"location"];
-            
-            
-           CLLocation *location=[[CLLocation alloc]initWithLatitude:[[loc_dict objectForKey:@"lat"] floatValue] longitude:[[loc_dict objectForKey:@"lng"] floatValue]];
-           
-            [self insertMarkerToMap:location.coordinate text:[dictResponce objectForKey:@"name"] click_id:[self createRandomName] title:[dictResponce objectForKey:@"name"]];
-            
-            
-        }
+    
+    for (int i = 0; i<[arrayResp count]; i++) {
+        
+        NSDictionary *dictResponce = [arrayResp objectAtIndex:i];
+        NSDictionary * loc_dict=[[dictResponce objectForKey:@"geometry"] objectForKey:@"location"];
+        
+        
+        CLLocation *location=[[CLLocation alloc]initWithLatitude:[[loc_dict objectForKey:@"lat"] floatValue] longitude:[[loc_dict objectForKey:@"lng"] floatValue]];
+        
+        [self insertMarkerToMap:location.coordinate text:[dictResponce objectForKey:@"name"] click_id:[self createRandomName] title:[dictResponce objectForKey:@"name"]];
+        
+        
+    }
     
 }
 
@@ -460,11 +499,11 @@
     
     NSString *command = [NSString stringWithFormat:@"%@%@", serverURL, placesCommand];
     
-//    AppDelegate* app=[[UIApplication sharedApplication] delegate];
-//    CLLocation * last_loc= [[app locationProvider]lastCoordinates];
+    //    AppDelegate* app=[[UIApplication sharedApplication] delegate];
+    //    CLLocation * last_loc= [[app locationProvider]lastCoordinates];
     
     
-   // NSMutableDictionary *params = [@{@"lng":[NSString stringWithFormat:@"%f",last_loc.coordinate.longitude], @"lat":[NSString stringWithFormat:@"%f",last_loc.coordinate.latitude], @"radius":[NSString stringWithFormat:@"%d",radiusCircle]} mutableCopy];
+    // NSMutableDictionary *params = [@{@"lng":[NSString stringWithFormat:@"%f",last_loc.coordinate.longitude], @"lat":[NSString stringWithFormat:@"%f",last_loc.coordinate.latitude], @"radius":[NSString stringWithFormat:@"%d",radiusCircle]} mutableCopy];
     NSMutableDictionary *params = [@{@"lng":[NSString stringWithFormat:@"%f",lon], @"lat":[NSString stringWithFormat:@"%f",lat], @"radius":[NSString stringWithFormat:@"%d",radiusCircle]} mutableCopy];
     
     [params setValue:[NSString stringWithString:name] forKey:@"name"];
@@ -475,9 +514,9 @@
     [networkHelper command:command params:params completionBlock:^(id responce, NSError *error) {
         
         NSLog(@"%@",responce);
-
+        
         [self jsonParseForDraw:responce];
-    
+        
     }];
     
 }
@@ -485,7 +524,7 @@
 
 - (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
     NSLog(@"textDidChange %@",searchText);
-  
+    
 }
 
 - (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar
@@ -503,24 +542,34 @@
             [self setCommandAndParamsWithName:searchBar.text andCategory:@""];
         }
     }
-   
     
     
-//    NSString *command = [NSString stringWithFormat:@"%@%@", serverURL, placesCommand];
-//    NSMutableDictionary *params = [@{@"lng":@"37.618423", @"lat":@"55.751244", @"radius":@"10000"} mutableCopy];
-//    [params setValue:[NSString stringWithString:searchBar.text] forKey:@"name"];
-//    
-//    
-//    URLNetworkHelper *networkHelper = [[URLNetworkHelper alloc] init];
-//    [networkHelper command:command params:params completionBlock:^(id responce, NSError *error) {
-//        NSLog(@"%@",responce);
-//    }];
+    
+    //    NSString *command = [NSString stringWithFormat:@"%@%@", serverURL, placesCommand];
+    //    NSMutableDictionary *params = [@{@"lng":@"37.618423", @"lat":@"55.751244", @"radius":@"10000"} mutableCopy];
+    //    [params setValue:[NSString stringWithString:searchBar.text] forKey:@"name"];
+    //
+    //
+    //    URLNetworkHelper *networkHelper = [[URLNetworkHelper alloc] init];
+    //    [networkHelper command:command params:params completionBlock:^(id responce, NSError *error) {
+    //        NSLog(@"%@",responce);
+    //    }];
 }
+
 
 - (void)searchBarTextDidBeginEditing:(UISearchBar *)aSearchBar {
     
     [aSearchBar setShowsCancelButton:YES animated:YES];
-    //[aSearchBar setShowsSearchResultsButton:YES];
+    
+    UIView* view=aSearchBar.subviews[0];
+    for (UIView *subView in view.subviews) {
+        if ([subView isKindOfClass:[UIButton class]]) {
+            UIButton *cancelButton = (UIButton*)subView;
+            
+            [cancelButton setTitle:[GLang getString: @"Places.searchBar.title"] forState:UIControlStateNormal];
+        }
+    }
+    
 }
 
 - (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
@@ -537,13 +586,13 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
